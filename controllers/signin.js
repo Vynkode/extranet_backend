6d0fdf24ee34bd75cd0f01c79ad5ed026c9446b5
Vynkode <kevinmuqu@me.com>
2020-12-31 11:40:21 +0100

    Not hash

--- controllers/signin.js
+++ controllers/signin.js
@@ -3,25 +3,34 @@ const handleSignin = (db, bcrypt) => (req, res) => {
   if (!email || !password) {
     return res.status(400).json('incorrect form submission');
   }
-  db.select('email', 'hash')
-    .from('login')
+
+  db.select('*')
+    .from('clientes_direcciones as cd')
     .where('email', '=', email)
-    .then((data) => {
-      const isValid = bcrypt.compareSync(password, data[0].hash);
-      if (isValid) {
-        return db
-          .select('*')
-          .from('clientes_direcciones as cd')
-          .where('email', '=', email)
-          .then((user) => {
-            res.json(user[0]);
-          })
-          .catch((err) => res.status(400).json('unable to get user'));
-      } else {
-        res.status(400).json('wrong credentials');
-      }
+    .then((user) => {
+      res.json(user[0]);
     })
     .catch((err) => res.status(400).json('wrong credentials'));
+
+  //   db.select('email', 'hash')
+  //     .from('login')
+  //     .where('email', '=', email)
+  //     .then((data) => {
+  //       const isValid = bcrypt.compareSync(password, data[0].hash);
+  //       if (isValid) {
+  //         return db
+  //           .select('*')
+  //           .from('clientes_direcciones as cd')
+  //           .where('email', '=', email)
+  //           .then((user) => {
+  //             res.json(user[0]);
+  //           })
+  //           .catch((err) => res.status(400).json('unable to get user'));
+  //       } else {
+  //         res.status(400).json('wrong credentials');
+  //       }
+  //     })
+  //     .catch((err) => res.status(400).json('wrong credentials'));
 };
 
 module.exports = {
