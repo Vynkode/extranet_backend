372b7de8f1ba6154975616a6dd83f7d7702ad1d2
Vynkode <kevinmuqu@me.com>
2021-01-04 10:17:51 +0100

    testing 2

--- controllers/test.js
+++ controllers/test.js
@@ -1,7 +1,8 @@
 const test = (db) => async (req, res) => {
   db('reparaciones')
     .select('*')
-    .then((data) => res.json(data));
+    .then((data) => res.json(data))
+    .catch((err) => res.status(200).json(err));
 };
 
 module.exports = { test: test };
