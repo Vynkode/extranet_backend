37558329645618b25f2c05b232aa6fe8127bb039
Vynkode <kevinmuqu@me.com>
2021-01-07 12:04:58 +0100

    fix data

--- controllers/repairs.js
+++ controllers/repairs.js
@@ -45,7 +45,7 @@ const handleWorkshopRepairs = (db) => async (req, res) => {
         element.f_reparacion = null;
       }
 
-      element.coste = parseInt(element.coste);
+      element.coste = parseFloat(element.coste);
 
       if (element.tipo_reparacion === '1') {
         element.tipo_reparacion = 'No Garantía';
