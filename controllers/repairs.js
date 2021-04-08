23794cf6b97e9c71fb94230aec42a6cc81ea4fe8
Vynkode <kevinmuqu@me.com>
2021-04-08 16:11:23 +0200

    Improving api requests

--- controllers/repairs.js
+++ controllers/repairs.js
@@ -1,7 +1,7 @@
 // const stream = require('stream');
 const moment = require('moment');
 
-const handleWorkshopRepairs = (db) => async (req, res) => {
+const handleWorkshopRepairs = db => async (req, res) => {
   const { email } = req.params;
 
   try {
@@ -33,82 +33,108 @@ const handleWorkshopRepairs = (db) => async (req, res) => {
         'r.agencia',
         'r.f_entrega',
         'r.proceso',
-        'r.estado',
-        // 'ta.accesorio1'
+        'r.estado'
       )
       .join('clientes_direcciones as cd', function () {
-        this.on('cd.codigo', '=', 'r.codigo_envio').andOn('cd.nombre', '=', 'r.nombre');
+        this.on('cd.codigo', '=', 'r.codigo_envio').andOn(
+          'cd.nombre',
+          '=',
+          'r.nombre'
+        );
       })
-      // .join('tipos_de_aparatos as ta', function () {
-      //   this.on('ta.tipo_aparato', '=', 'r.tipo_aparato');
-      // })
-      .where((builder) => {
-        builder.where('cd.email', '=', email).where('r.operario', '!=', 'INMA').where('r.proceso', '!=', '8');
+      .where(builder => {
+        builder
+          .where('cd.email', '=', email)
+          .where('r.operario', '!=', 'INMA')
+          .where('r.proceso', '!=', '8');
       })
       .orderBy('r.f_entrada', 'desc');
 
     const count = repairs.length;
     console.log(count);
 
-    repairs.forEach((element) => {
+    repairs.forEach(element => {
       if (element.foto_entrada) {
-        element.foto_entrada = Buffer.from(element.foto_entrada).toString('base64');
+        element.foto_entrada = Buffer.from(element.foto_entrada).toString(
+          'base64'
+        );
       }
 
       if (element.tipo_reparacion === '1') {
         element.tipo_reparacion = 'No Garantía';
-        element.fecha_compra = null
+        element.fecha_compra = null;
       } else if (element.tipo_reparacion === '2') {
         element.tipo_reparacion = 'Garantía';
         element.fecha_compra = moment(element.fecha_compra).format('DD/MM/YY');
       } else if (element.tipo_reparacion === '3') {
         element.tipo_reparacion = 'Reclamación';
-        element.fecha_compra = null
+        element.fecha_compra = null;
       }
 
       element.f_entrada = moment(element.f_entrada).format('DD/MM/YY');
 
       const averiaString = element.averia ? element.averia.toString() : '';
       if (averiaString) {
-        element.averia = averiaString[0].toUpperCase() + averiaString.slice(1).toLowerCase();
+        element.averia =
+          averiaString[0].toUpperCase() + averiaString.slice(1).toLowerCase();
       } else {
         element.averia = '';
       }
 
-      const observacionesString = element.observaciones ? element.observaciones.toString() : '';
+      const observacionesString = element.observaciones
+        ? element.observaciones.toString()
+        : '';
       if (observacionesString) {
-        element.observaciones =  observacionesString[0].toUpperCase() + observacionesString.slice(1).toLowerCase();
+        element.observaciones =
+          observacionesString[0].toUpperCase() +
+          observacionesString.slice(1).toLowerCase();
       } else {
         element.observaciones = '';
       }
 
-      if (element.presupuestar === '' || element.presupuestar === 'N' || !element.presupuestar) {
+      if (
+        element.presupuestar === '' ||
+        element.presupuestar === 'N' ||
+        !element.presupuestar
+      ) {
         element.presupuestar = 'No';
       } else {
         element.presupuestar = 'Sí';
       }
 
       if (element.f_presupuesto) {
-        element.f_presupuesto = moment(element.f_presupuesto).format('DD/MM/YY');
+        element.f_presupuesto = moment(element.f_presupuesto).format(
+          'DD/MM/YY'
+        );
       } else {
         element.f_presupuesto = null;
       }
 
       if (element.f_respuesta_ppto) {
-        element.f_respuesta_ppto = moment(element.f_respuesta_ppto).format('DD/MM/YY');
+        element.f_respuesta_ppto = moment(element.f_respuesta_ppto).format(
+          'DD/MM/YY'
+        );
       } else {
         element.f_respuesta_ppto = null;
       }
 
-      if (element.rechazado === '' || element.rechazado === 'N' || !element.rechazado) {
+      if (
+        element.rechazado === '' ||
+        element.rechazado === 'N' ||
+        !element.rechazado
+      ) {
         element.rechazado = 'No';
       } else {
         element.rechazado = 'Sí';
       }
 
-      const presupuestoString = element.presupuesto ? element.presupuesto.toString() : '';
+      const presupuestoString = element.presupuesto
+        ? element.presupuesto.toString()
+        : '';
       if (presupuestoString) {
-        element.presupuesto = presupuestoString[0].toUpperCase() + presupuestoString.slice(1).toLowerCase();
+        element.presupuesto =
+          presupuestoString[0].toUpperCase() +
+          presupuestoString.slice(1).toLowerCase();
       } else {
         element.presupuesto = '';
       }
@@ -122,9 +148,13 @@ const handleWorkshopRepairs = (db) => async (req, res) => {
         element.f_reparacion = null;
       }
 
-      const reparacionString = element.reparacion ? element.reparacion.toString() : '';
+      const reparacionString = element.reparacion
+        ? element.reparacion.toString()
+        : '';
       if (reparacionString) {
-        element.reparacion = reparacionString[0].toUpperCase() + reparacionString.slice(1).toLowerCase();
+        element.reparacion =
+          reparacionString[0].toUpperCase() +
+          reparacionString.slice(1).toLowerCase();
       } else {
         element.reparacion = '';
       }
@@ -132,7 +162,13 @@ const handleWorkshopRepairs = (db) => async (req, res) => {
       // element.f_base_imponible = parseFloat(element.f_base_imponible).toFixed(2);
       element.f_liquido = parseFloat(element.f_liquido).toFixed(2);
 
-      if (element.agencia === 'SEUR' || element.agencia === 'CORREOS' || element.agencia === 'ENVIALIA' || element.agencia === 'SUS MEDIOS' || element.agencia === 'SUS MEDI') {
+      if (
+        element.agencia === 'SEUR' ||
+        element.agencia === 'CORREOS' ||
+        element.agencia === 'ENVIALIA' ||
+        element.agencia === 'SUS MEDIOS' ||
+        element.agencia === 'SUS MEDI'
+      ) {
         element.agencia = 'ENVÍO';
       } else {
         element.agencia = 'RECOGIDA';
@@ -151,7 +187,7 @@ const handleWorkshopRepairs = (db) => async (req, res) => {
   }
 };
 
-const handleClosedRepairs = (db) => async (req, res) => {
+const handleClosedRepairs = db => async (req, res) => {
   const { email } = req.params;
 
   try {
@@ -185,10 +221,17 @@ const handleClosedRepairs = (db) => async (req, res) => {
         'r.estado'
       )
       .join('clientes_direcciones as cd', function () {
-        this.on('cd.codigo', '=', 'r.codigo_envio').andOn('cd.nombre', '=', 'r.nombre');
+        this.on('cd.codigo', '=', 'r.codigo_envio').andOn(
+          'cd.nombre',
+          '=',
+          'r.nombre'
+        );
       })
-      .where((builder) => {
-        builder.where('cd.email', '=', email).where('r.operario', '!=', 'INMA').where('r.proceso', '=', '8');
+      .where(builder => {
+        builder
+          .where('cd.email', '=', email)
+          .where('r.operario', '!=', 'INMA')
+          .where('r.proceso', '=', '8');
       })
       .limit(500)
       .orderBy('r.f_entrada', 'desc');
@@ -196,9 +239,11 @@ const handleClosedRepairs = (db) => async (req, res) => {
     const count = repairs.length;
     // console.log(count);
 
-    repairs.forEach((element) => {
+    repairs.forEach(element => {
       if (element.foto_entrada) {
-        element.foto_entrada = Buffer.from(element.foto_entrada).toString('base64');
+        element.foto_entrada = Buffer.from(element.foto_entrada).toString(
+          'base64'
+        );
       }
 
       if (element.tipo_reparacion === '1') {
@@ -222,45 +267,66 @@ const handleClosedRepairs = (db) => async (req, res) => {
 
       const averiaString = element.averia ? element.averia.toString() : '';
       if (averiaString) {
-        element.averia = averiaString[0].toUpperCase() + averiaString.slice(1).toLowerCase();
+        element.averia =
+          averiaString[0].toUpperCase() + averiaString.slice(1).toLowerCase();
       } else {
         element.averia = '';
       }
 
-      const observacionesString = element.observaciones ? element.observaciones.toString() : '';
+      const observacionesString = element.observaciones
+        ? element.observaciones.toString()
+        : '';
       if (observacionesString) {
-        element.observaciones =  observacionesString[0].toUpperCase() + observacionesString.slice(1).toLowerCase();
+        element.observaciones =
+          observacionesString[0].toUpperCase() +
+          observacionesString.slice(1).toLowerCase();
       } else {
         element.observaciones = '';
       }
 
-      if (element.presupuestar === '' || element.presupuestar === 'N' || !element.presupuestar) {
+      if (
+        element.presupuestar === '' ||
+        element.presupuestar === 'N' ||
+        !element.presupuestar
+      ) {
         element.presupuestar = 'No';
       } else {
         element.presupuestar = 'Sí';
       }
 
       if (element.f_presupuesto) {
-        element.f_presupuesto = moment(element.f_presupuesto).format('DD/MM/YY');
+        element.f_presupuesto = moment(element.f_presupuesto).format(
+          'DD/MM/YY'
+        );
       } else {
         element.f_presupuesto = null;
       }
 
       if (element.f_respuesta_ppto) {
-        element.f_respuesta_ppto = moment(element.f_respuesta_ppto).format('DD/MM/YY');
+        element.f_respuesta_ppto = moment(element.f_respuesta_ppto).format(
+          'DD/MM/YY'
+        );
       } else {
         element.f_respuesta_ppto = null;
       }
 
-      if (element.rechazado === '' || element.rechazado === 'N' || !element.rechazado) {
+      if (
+        element.rechazado === '' ||
+        element.rechazado === 'N' ||
+        !element.rechazado
+      ) {
         element.rechazado = 'No';
       } else {
         element.rechazado = 'Sí';
       }
 
-      const presupuestoString = element.presupuesto ? element.presupuesto.toString() : '';
+      const presupuestoString = element.presupuesto
+        ? element.presupuesto.toString()
+        : '';
       if (presupuestoString) {
-        element.presupuesto = presupuestoString[0].toUpperCase() + presupuestoString.slice(1).toLowerCase();
+        element.presupuesto =
+          presupuestoString[0].toUpperCase() +
+          presupuestoString.slice(1).toLowerCase();
       } else {
         element.presupuesto = '';
       }
@@ -274,9 +340,13 @@ const handleClosedRepairs = (db) => async (req, res) => {
         element.f_reparacion = null;
       }
 
-      const reparacionString = element.reparacion ? element.reparacion.toString() : '';
+      const reparacionString = element.reparacion
+        ? element.reparacion.toString()
+        : '';
       if (reparacionString) {
-        element.reparacion = reparacionString[0].toUpperCase() + reparacionString.slice(1).toLowerCase();
+        element.reparacion =
+          reparacionString[0].toUpperCase() +
+          reparacionString.slice(1).toLowerCase();
       } else {
         element.reparacion = '';
       }
@@ -284,7 +354,13 @@ const handleClosedRepairs = (db) => async (req, res) => {
       // element.f_base_imponible = parseFloat(element.f_base_imponible).toFixed(2);
       element.f_liquido = parseFloat(element.f_liquido).toFixed(2);
 
-      if (element.agencia === 'SEUR' || element.agencia === 'CORREOS' || element.agencia === 'ENVIALIA' || element.agencia === 'SUS MEDIOS' || element.agencia === 'SUS MEDI') {
+      if (
+        element.agencia === 'SEUR' ||
+        element.agencia === 'CORREOS' ||
+        element.agencia === 'ENVIALIA' ||
+        element.agencia === 'SUS MEDIOS' ||
+        element.agencia === 'SUS MEDI'
+      ) {
         element.envio = 'ENVÍO';
       } else {
         element.envio = 'RECOGIDA';
