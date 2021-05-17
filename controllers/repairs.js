020cd6f6839e639ddc7800a6f1a970a99892ff1d
Vynkode <kevinmuqu@me.com>
2021-05-17 13:34:19 +0200

    Beta testing up

--- controllers/repairs.js
+++ controllers/repairs.js
@@ -54,7 +54,7 @@ const handleRepairs = db => async (req, res) => {
         builder
           .where('r.codigo_contable', '=', codigo)
           .where('r.codigo_envio', '=', dir)
-          .where('r.operario', '!=', 'INMA')
+          .where(db.raw('(r.operario != INMA or r.operario is null)'))
           .where('r.proceso', find, 8);
       })
       .limit(100)
