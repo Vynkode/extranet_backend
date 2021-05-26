bd20e3b875d0e0d3987a953ad4611a6ae0962f5a
Vynkode <kevinmuqu@me.com>
2021-05-26 14:03:40 +0200

    Beta testing up

--- controllers/repairBudget.js
+++ controllers/repairBudget.js
@@ -2,7 +2,6 @@ const moment = require('moment');
 
 const handleAcceptBudget = (db, bcrypt) => async (req, res) => {
   const { numero } = req.body;
-  // const modDate = moment().format('YYYY-MM-DD hh:mm:ss.SSS');
   const date = moment().format('YYYY-MM-DD');
   try {
     const repair = await db('reparaciones')
@@ -10,10 +9,8 @@ const handleAcceptBudget = (db, bcrypt) => async (req, res) => {
       .update({
         f_respuesta_ppto: date,
         estado: 4,
-        // ultima_modificacion: modDate
       })
       .returning(['numero', 'f_respuesta_ppto']);
-    // console.log([modDate, date]);
     if (!repair.length)
       throw new Error('No se ha podido actualizar correctamente la reparación');
     return res
@@ -28,19 +25,16 @@ const handleAcceptBudget = (db, bcrypt) => async (req, res) => {
 
 const handleRejectBudget = (db, bcrypt) => async (req, res) => {
   const { numero } = req.body;
-  // const modDate = moment().format('YYYY-MM-DD hh:mm:ss.SSS');
   const date = moment().format('YYYY-MM-DD');
   try {
     const repair = await db('reparaciones')
       .where('numero', '=', numero)
       .update({
         f_respuesta_ppto: date,
-        // ultima_modificacion: modDate,
         rechazado: 'S',
         estado: 3,
       })
       .returning(['numero', 'f_respuesta_ppto', 'rechazado']);
-    // console.log([modDate, date]);
     if (!repair.length)
       throw new Error('No se ha podido actualizar correctamente la reparación');
     return res
