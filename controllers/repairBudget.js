4b0752aaad1ab88588c7fd11b5d9ceb9e0ebbba1
Vynkode <kevinmuqu@me.com>
2021-05-26 13:06:44 +0200

    Beta testing up

--- controllers/repairBudget.js
+++ controllers/repairBudget.js
@@ -4,27 +4,26 @@ const handleAcceptBudget = (db, bcrypt) => async (req, res) => {
   const { numero } = req.body;
   // const modDate = moment().format('YYYY-MM-DD hh:mm:ss.SSS');
   const date = moment().format('YYYY-MM-DD');
-  console.log([numero, date]);
-  // try {
-  //   const repair = await db('reparaciones')
-  //     .where('numero', '=', numero)
-  //     .update({
-  //       f_respuesta_ppto: date,
-  //       estado: 4,
-  //       // ultima_modificacion: modDate
-  //     })
-  //     .returning(['numero', 'f_respuesta_ppto']);
-  //   // console.log([modDate, date]);
-  //   if (!repair.length)
-  //     throw new Error('No se ha podido actualizar correctamente la reparación');
-  //   return res
-  //     .status(200)
-  //     .json(
-  //       `El presupuesto ${repair[0].numero} se ha aceptado a fecha ${date}`
-  //     );
-  // } catch (e) {
-  //   return res.status(400).json(e.message);
-  // }
+  try {
+    const repair = await db('reparaciones')
+      .where('numero', '=', numero)
+      .update({
+        f_respuesta_ppto: date,
+        estado: 4,
+        // ultima_modificacion: modDate
+      })
+      .returning(['numero', 'f_respuesta_ppto']);
+    // console.log([modDate, date]);
+    if (!repair.length)
+      throw new Error('No se ha podido actualizar correctamente la reparación');
+    return res
+      .status(200)
+      .json(
+        `El presupuesto ${repair[0].numero} se ha aceptado a fecha ${date}`
+      );
+  } catch (e) {
+    return res.status(400).json(e.message);
+  }
 };
 
 const handleRejectBudget = (db, bcrypt) => async (req, res) => {
@@ -38,7 +37,7 @@ const handleRejectBudget = (db, bcrypt) => async (req, res) => {
         f_respuesta_ppto: date,
         // ultima_modificacion: modDate,
         rechazado: 'S',
-        estado: 4,
+        estado: 3,
       })
       .returning(['numero', 'f_respuesta_ppto', 'rechazado']);
     // console.log([modDate, date]);
