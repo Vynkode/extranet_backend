e9494e1060990557f593ea618db1526220eaebdf
Vynkode <kevinmuqu@me.com>
2021-06-25 08:31:20 +0200

    Send email budget

--- controllers/test.js
+++ controllers/test.js
@@ -1,4 +1,5 @@
 const moment = require('moment');
+const jwt = require('jsonwebtoken');
 
 const reparaciones = db => (req, res) => {
   const obs = [];
@@ -69,53 +70,55 @@ const direcciones = db => (req, res) => {
     .catch(err => res.status(400).json(err));
 };
 
-const pruebaSignin = (db, bcrypt) => async (req, res) => {
+const handleSignin = (db, bcrypt) => async (req, res) => {
   const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
+  const date = moment().format('YYYY-MM-DD hh:mm:ss.SSS');
   try {
     const [login] = await db
-      .select('codigo_contable', 'email', 'codigo', 'hash')
+      .select('codigo_contable', 'email', 'codigo', 'hash', 'first_time')
       .from('login_extranet')
       .where('email', '=', email);
-    console.log(login);
     if (!login.email) throw new Error();
     const isValid = bcrypt.compareSync(password, login.hash);
-    let user;
     if (isValid) {
-      try {
-        const [data] = await db
-          .select(
-            'cd.codigo',
-            'c.codigo_contable',
-            'cd.nombre',
-            'c.razon_social',
-            'c.nif',
-            'cd.email',
-            'cd.telefono1',
-            'cd.calle',
-            'cd.distrito',
-            'cd.ciudad',
-            'cd.provincia',
-            'cd.contacto',
-            'cd.fax',
-            'c.distribuidor'
-          )
-          .from('clientes_direcciones as cd')
-          .where('cd.email', '=', email)
-          .andWhere('cd.codigo', '=', login.codigo)
-          .andWhere('c.codigo_contable', '=', login.codigo_contable)
-          .join('clientes as c', 'cd.nombre', '=', 'c.nombre');
-        data.id = `${data.codigo_contable}${data.codigo}`;
-        delete data.codigo;
-        delete data.codigo_contable;
-        console.log(data);
-        user = data;
-      } catch (e) {
-        return res.status(400).json('unable to get user');
-      }
+      const [user] = await db
+        .select(
+          'cd.codigo',
+          'c.codigo_contable',
+          'cd.nombre',
+          'c.razon_social',
+          'c.nif',
+          'cd.email',
+          'cd.telefono1',
+          'cd.calle',
+          'cd.distrito',
+          'cd.ciudad',
+          'cd.provincia',
+          'cd.contacto',
+          'cd.fax',
+          'c.distribuidor'
+        )
+        .from('clientes_direcciones as cd')
+        .where('cd.email', '=', email)
+        .andWhere('cd.codigo', '=', login.codigo)
+        .andWhere('c.codigo_contable', '=', login.codigo_contable)
+        .join('clientes as c', 'cd.nombre', '=', 'c.nombre');
+      user.id = `${user.codigo_contable}${user.codigo}`;
+      delete user.codigo;
+      delete user.codigo_contable;
+      user.first_time = login.first_time;
+      if (!user.distribuidor) user.nombre = user.nombre.split('-')[0].trim();
+      console.log(user.nombre);
+      console.log(`${date}: Acceso correcto del usuario ${user.id}`);
+      const token = jwt.sign({ id: user.id }, 'secretmgvsecret');
+      return res.status(200).json({ user, token });
     } else {
+      console.log(`${date}: Acceso erroneo del usuario ${user.id}`);
       return res.status(400).json('wrong credentials');
     }
-    return res.status(205).json(user);
   } catch (e) {
     return res.status(400).json('wrong credentials');
   }
@@ -125,5 +128,5 @@ module.exports = {
   reparaciones: reparaciones,
   clientes: clientes,
   direcciones: direcciones,
-  pruebaSignin: pruebaSignin,
+  pruebaSignin: handleSignin,
 };
